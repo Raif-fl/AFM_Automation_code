9cad956ed3a5b95be72fb08efbcbeb1c59a07545
Keegan <flanagan.keegan16@gmail.com>
2022-04-08 18:12:50 -0700

    completed implementation of skeleton

--- tools.py
+++ tools.py
@@ -16,35 +16,26 @@ import skimage.morphology as sk_m
 from fil_finder import FilFinder2D
 from astropy import units as u
 from radfil import radfil_class, styles
+from shapely.geometry import Polygon
 
 def atoi(text):
+    '''
+    A function which supports 
+    '''
     return int(text) if text.isdigit() else text
 
 def natural_keys(text):
     '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
+    A function which allows me to sort input file names by timepoint/
     '''
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
-def outlines_list(masks):
-    """ get outlines of masks as a list to loop over for plotting """
-    outpix=[]
-    for n in np.unique(masks)[1:]:
-        mn = masks==n
-        if mn.sum() > 0:
-            contours = cv2.findContours(mn.astype(np.uint8), mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-            contours = contours[-2]
-            cmax = np.argmax([c.shape[0] for c in contours])
-            pix = contours[cmax].astype(int).squeeze()
-            if len(pix)>4:
-                outpix.append(pix)
-            else:
-                outpix.append(np.zeros((0,2)))
-    return outpix
-
 def get_boxes(outl):
+    '''
+    A function used to compute the bounding boxes for all of the outlines from
+    a single image. 
+    '''
+    # Initialize a list of bounding boxes. 
     boxes = []
     for o in outl:
         # compute the rotated bounding box of the contour
@@ -53,38 +44,58 @@ def get_boxes(outl):
         box = np.array(box, dtype="int")
         # order the points in the contour such that they appear
         # in top-left, top-right, bottom-right, and bottom-left
-        # order, then draw the outline of the rotated bounding
-        # box
+        # order
         box = perspective.order_points(box)
-        # draw the bounding boxes and centers. 
+        # append the bounding box to our list.
         boxes.append(box)
     return boxes
 
-def get_overlap(outl, centers):
+def get_overlap(outl, boxes):
+    '''
+    A function which iterates over all cell outlines in an image and detects how much of the cell's
+    surface is overlapping another cell's. 
+    '''
+    # initialize a list to save the overlaps
     cell_overlaps = []
-    for out_cell, cen_cell in zip(outl, centers):
+    
+    # Loop through each cell in the image. 
+    for out_cell, box_cell in zip(outl, boxes):
         adj = 0
-        for out_oth, cen_oth in zip(outl, centers):
-            cent_dist = dist.euclidean(cen_cell,cen_oth)
-            # centroid distance cuttod=ff = 150 pixels. 
-            if cent_dist > 150 or cent_dist == 0:
+        # For each cell in the image, loop through everyother cell in the image.
+        for out_oth, box_oth in zip(outl, boxes):
+            # detect if the two cells have overlapping bounding boxes.
+            # If bounding boxes do not overlap, skip to next cell. 
+            p1 = Polygon(box_cell)
+            p2 = Polygon(box_oth)
+            if p1.intersects(p2) == False or p1 == p2:
                 continue
+            # For each pixel in cell outline, measure the distance to the pixels
+            # in the other cell outline. If said distance is zero, then the pixels
+            # are overlapping. 
             for pixel1 in out_cell:
                 for pixel2 in out_oth:
                     distx = np.abs(pixel1[0]-pixel2[0])
                     disty = np.abs(pixel1[1]-pixel2[1])
                     if distx + disty == 1:
                         adj = adj + 1
+        # append the overlap information to our list.
         cell_overlaps.append(adj)
     return cell_overlaps 
 
 def peri_area(outl_list):
+    '''
+    An iterative function that calculates the perimeter and area for all cell outlines within a 
+    list of images. 
+    '''
+    # Initialize lists to hold the perimeter and area information for all images.
     per_list = []
     area_list = []
     for outl in outl_list:
+        # Initialize lists to hold the perimeter and area infromation within each individual image. 
         peris = []
         areas = []
         for o in outl:
+            # Calculate the perimeter and area for each outline
             peris.append(cv2.arcLength(o, True))
             areas.append(cv2.contourArea(o))
         per_list.append(peris)
@@ -92,6 +103,9 @@ def peri_area(outl_list):
     return per_list, area_list
 
 def save_ind_masks(path, IDs_list, outl_new_list, time_list, img_list):
+    '''
+    A function which 
+    '''
     for ID_set, outl, time, img in zip(IDs_list, outl_new_list, time_list, img_list):
         if os.path.exists(path + str(time)) == False:
             os.makedirs(path + str(time))
@@ -132,6 +146,7 @@ def radobj_maker(path, IDs_list, time_list):
     radobj_list = []
     for ID_set, time in zip(IDs_list, time_list):
         radobj_set = []
+        print(time)
         for idx in (ID_set):
             # Load the image and convert to grayscale. 
             fil_image =imageio.imread(path + str(time) + "/cells/cell_" + str(idx) + ".png")
@@ -143,8 +158,16 @@ def radobj_maker(path, IDs_list, time_list):
             fil_mask = np.array(fil_mask, dtype=bool)
 
             ###!!!### Replace with a call to Hasti's stuff
-            fil_spine = sk_m.skeletonize(fil_mask)
-            fil_spine = np.array(fil_spine, dtype=bool)
+            spine = np.loadtxt(open(path + str(time) + "/skeletons/matrix_" + str(idx) + ".csv", "rb"), delimiter=",", skiprows=1)[1:-2].astype(int)
+            dbl = int((len(spine)-2)/2)
+            cut_spine = spine[1:-2]
+
+            x, y = [i[0] for i in cut_spine], [i[1] for i in cut_spine]
+
+            fil_spine = np.zeros(fil_mask.shape, dtype = bool)
+
+            for i in range(len(cut_spine)):
+                fil_spine[y[i], x[i]] = True
             
             # Use radfil to create a radial profil object and then append this object to a list.
             # We will use the data stored in this object to calculate height and radial profile along the
@@ -163,7 +186,7 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return idx
 
-def radobj_extractor(radobj_list):
+def radobj_extractor(radobj_list, h_conv = 3.9215686274509802):
     '''
     This function iterates through a list of radfil objects and extracts two meaningful outputs for each cell.
     The first is a list that details the diameter of the cell along the medial axis, and the second is a list
@@ -180,9 +203,8 @@ def radobj_extractor(radobj_list):
             height = []
             for dist, prof in zip(radobj.dictionary_cuts['distance'],radobj.dictionary_cuts['profile']) :
                 diam.append(np.abs(dist[0] - dist[-1]))
-                # it is not argmin, it is closest to zero! 
                 ind = find_nearest(dist,0)
-                height.append(prof[ind])
+                height.append(prof[ind]*h_conv)
             diam_set.append(diam)
             height_set.append(height)
         diam_list.append(diam_set)
@@ -190,6 +212,46 @@ def radobj_extractor(radobj_list):
     return diam_list, height_list
 
 def skeleton_length(path, IDs_list, time_list):
+    '''
+    This iterative function loads up a set of skeletons for individual cells which were created 
+    from a set of parent images taken at different timepoints. For each of these skeletons, it uses
+    the filfinder package to convert the skeleton to a filfinder skeleton. This fairly complicated
+    process is done simply so that the length of the skeleton can be recorded. 
+    '''
+    length_list = []
+    for ID_set, time in zip(IDs_list, time_list):
+        length_set = []
+        for idx in (ID_set):
+             ###!!!### Replace with a call to Hasti's stuff
+            spine = np.loadtxt(open(path + str(time) + "/skeletons/matrix_" + str(idx) + ".csv", "rb"), delimiter=",", skiprows=1)[1:-2].astype(int)
+            dbl = int((len(spine)-2)/2)
+            cut_spine = spine[1:-2]
+
+            x, y = [i[0] for i in cut_spine], [i[1] for i in cut_spine]
+
+            fil_mask=imageio.imread(path + str(time) + "/masks/mask_" + str(idx) + ".png")
+            fil_spine = np.zeros(fil_mask.shape, dtype = 'uint8')
+
+            for i in range(len(cut_spine)):
+                fil_spine[y[i], x[i]] = 1
+            fil_spine = cv2.cvtColor(fil_spine, cv2.COLOR_BGR2GRAY)
+            
+            fil = FilFinder2D(fil_spine, distance=250 * u.pix, mask=fil_spine)
+            fil.preprocess_image(flatten_percent=85)
+            fil.create_mask(border_masking=True, verbose=False,
+            use_existing_mask=True)
+            fil.medskel(verbose=False)
+            fil.analyze_skeletons(branch_thresh=40* u.pix, skel_thresh=10 * u.pix, prune_criteria='length')
+            if len(fil.lengths().value) > 0:
+                length = fil.lengths().value[0]
+            else:
+                length = np.nan
+        
+            length_set.append(length)
+        length_list.append(length_set)
+    return(length_list)
+
+def skeleton_length_old(path, IDs_list, time_list):
     '''
     This iterative function loads up a set of skeletons for individual cells which were created 
     from a set of parent images taken at different timepoints. For each of these skeletons, it uses
@@ -264,10 +326,10 @@ class CentroidTracker():
         # object is allowed to be marked as "disappeared" until we
         # need to deregister the object from tracking
         self.maxDisappeared = maxDisappeared
-    def register(self, centroid, outline):
+    def register(self, rects, centroid, outline):
         # when registering an object we use the next available object
         # ID to store the centroid
-        self.objects[self.nextObjectID] = (centroid, outline)
+        self.objects[self.nextObjectID] = (rects, centroid, outline)
         self.disappeared[self.nextObjectID] = 0
         self.nextObjectID += 1
     def deregister(self, objectID):
@@ -287,8 +349,8 @@ class CentroidTracker():
         # if we are currently not tracking any objects take the input
         # centroids and register each of them
         if len(self.objects) == 0:
-            for i, o in zip(range(0, len(inputCentroids)), outls):
-                self.register(inputCentroids[i], o)
+            for r, i, o in zip(rects, range(0, len(inputCentroids)), outls):
+                self.register(r, inputCentroids[i], o)
         # otherwise, are are currently tracking objects so we need to
         # try to match the input centroids to existing object
         # centroids
@@ -298,7 +360,7 @@ class CentroidTracker():
             objectinfo = list(self.objects.values())
             objectCentroids = []
             for info in objectinfo:
-                objectCentroids.append(info[0])
+                objectCentroids.append(info[1])
             # compute the distance between each pair of object
             # centroids and input centroids, respectively -- our
             # goal will be to match an input centroid to an existing
@@ -331,7 +393,7 @@ class CentroidTracker():
                 # set its new centroid, and reset the disappeared
                 # counter
                 objectID = objectIDs[row]
-                self.objects[objectID] = (inputCentroids[col], outls[col])
+                self.objects[objectID] = (rects[col], inputCentroids[col], outls[col])
                 self.disappeared[objectID] = 0
                 # indicate that we have examined each of the row and
                 # column indexes, respectively
@@ -362,6 +424,6 @@ class CentroidTracker():
             # register each new input centroid as a trackable object
             else:
                 for col in unusedCols:
-                    self.register(inputCentroids[col], outls[col])
+                    self.register(rects[col], inputCentroids[col], outls[col])
         # return the set of trackable objects
         return self.objects
